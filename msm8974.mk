18426a4b20571166cf2e4e08192dd2d2cac73fa6
Paul Keith <javelinanddart@gmail.com>
2018-05-27 15:08:48 +0200

    msm8974-common: Add codec entries for ddp
    * media_codecs_ddp from G960USQU1ARBG
    
    Change-Id: Ic6bddce26e782652120335d7770b384b3f416f8b

--- msm8974.mk
+++ msm8974.mk
@@ -132,6 +132,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml  \
+    device/samsung/msm8974-common/configs/media_codecs_ddp.xml:system/etc/media_codecs_ddp.xml \
     device/samsung/msm8974-common/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # OMX
